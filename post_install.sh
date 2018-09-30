8803a03e6dc5166afb383eadca320aa7dce90937
rubenverhoef <ruben_verhoef@hotmail.com>
2018-09-30 19:50:33 +0200

    back to manual installation to get latest version

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+python2.7 -m pip install cheetah
+python2.7 -m pip install cryptography
+python2.7 -m pip install sabyenc
+
+cd / && curl -s https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest | grep "browser_download_url.*-src.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+tar -xvzf SABnzbd*.tar.gz -C /root
+rm SABnzbd*.tar.gz
+mv /root/SABnzbd* /root/SABnzbd
+
+chmod +x /etc/rc.d/sabnzbd
+
 sysrc 'sabnzbd_enable=YES'
 
 service sabnzbd start
