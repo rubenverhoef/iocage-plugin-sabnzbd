c7f571c96b12c722e6bc69159f18e68a6b3e9347
rubenverhoef <ruben_verhoef@hotmail.com>
2018-10-01 19:56:07 +0200

    manual installation

--- post_install.sh
+++ post_install.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-#python2.7 -m pip install --upgrade pip
-#python2.7 -m pip install cheetah
-#python2.7 -m pip install cryptography
-#python2.7 -m pip install sabyenc
+python2.7 -m pip install --upgrade pip
+python2.7 -m pip install cheetah
+python2.7 -m pip install cryptography
+python2.7 -m pip install sabyenc
 
-#cd / && curl -s https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest | grep "browser_download_url.*-src.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-#tar -xvzf SABnzbd*.tar.gz -C /root
-#rm SABnzbd*.tar.gz
-#mv /root/SABnzbd* /root/SABnzbd
+cd / && curl -s https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest | grep "browser_download_url.*-src.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+tar -xvzf SABnzbd*.tar.gz -C /usr/local/bin
+rm SABnzbd*.tar.gz
+mv /usr/local/bin/SABnzbd* /usr/local/bin/SABnzbd
 
-#chmod +x /etc/rc.d/sabnzbd
-
-#sysrc 'sabnzbd_enable=YES'
-
-#service sabnzbd start
+chmod +x /usr/local/etc/rc.d/sabnzbd
