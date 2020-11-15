5346c312be9604497a220febf79da3db23c666cf
Ruben Verhoef <ruben_verhoef@hotmail.com>
2020-11-15 15:53:59 +0100

    Fix issue in install script, requirments is present after downloading it

--- post_install.sh
+++ post_install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-python3.7 -m pip install wheel
-python3.7 -m pip install -r /root/SABnzbd/requirements.txt -U
-
 cd / && curl -s https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest | grep "browser_download_url.*-src.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 tar -xvzf SABnzbd*.tar.gz -C /root
 rm SABnzbd*.tar.gz
 mv /root/SABnzbd* /root/SABnzbd
+
+python3.7 -m pip install wheel
+python3.7 -m pip install -r /root/SABnzbd/requirements.txt -U
 
 chmod +x /usr/local/etc/rc.d/sabnzbd
